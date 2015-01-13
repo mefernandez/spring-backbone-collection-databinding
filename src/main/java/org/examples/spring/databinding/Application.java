aae79d9f634809292b97f5a2af575b1a3b3439d3
Mariano Fernández <mariano.fernandez@gmail.com>
2015-01-13 07:59:32 +0100

    Added Spring Boot

---
+++ src/main/java/org/examples/spring/databinding/Application.java
@@ -0,0 +1,14 @@
+package org.examples.spring.databinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
