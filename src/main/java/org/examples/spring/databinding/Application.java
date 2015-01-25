0d080f474b5c32d2ba6efaec92e70a49ab486e0e
Mariano Fernández <mariano.fernandez@gmail.com>
2015-01-25 14:16:35 +0100

    Try to deploy on Heroku

--- src/main/java/org/examples/spring/databinding/Application.java
+++ src/main/java/org/examples/spring/databinding/Application.java
@@ -2,13 +2,21 @@ package org.examples.spring.databinding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	/*
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootApplication.class);
+    }
+    */
 }
